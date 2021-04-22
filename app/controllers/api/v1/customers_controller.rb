43aec1c67fc7f508516c12e30c0531dfa7ba82fe
Rem Nikolsky <r3mnik@gmail.com>
2021-04-22 20:07:55 +0300

    Format response for index of customers
    -send only id and name of customers

--- app/controllers/api/v1/customers_controller.rb
+++ app/controllers/api/v1/customers_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::CustomersController < Api::V1::ApplicationController
   def index
-    customers = Customer.all
+    customers = Customer.pluck(:_id, :name).map{ |rec| {id: rec[0].to_s, name: rec[1]} }
     render json: customers, status: :ok
     #render json: {results: customers}.to_json, status: :ok
   end
